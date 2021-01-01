b60ede62298dde0ee467ddb8136044385b3ccce7
Sausan18 <76219106+Sausan18@users.noreply.github.com>
2021-01-01 16:44:23 +0700

    penambahan dan perubahan code

--- Promo.java
+++ Promo.java
@@ -1,17 +1,23 @@
 
 /**
  * @author (Sausan Nabilah)
- * @version (30/12/20)
+ * @version (1/01/21)
  */
 
 import java.util.Scanner;
 
-public class Promo extends Pelayanan
+public class Promo 
 {
+
+      private Pelayanan pelayanan;
+      private Penyimpanan penyimpanan;
+      private int biaya;
 
     Public Promo()
     {
-          Super();
+          pelayanan = new Pelayanan();
+          penyimpanan = new Penyimpanan();
+          biaya = 0;
     }  
     
     public void Beranda()
@@ -26,31 +32,125 @@ public class Promo extends Pelayanan
 
     public void promoDB()
     {
-          Scanner p = new Scanner (System.in);     
-          int pilihanDonat = 0;
+            System.out.println(" Selamat anda mendapatkan promo beli 3 dessert box dapat 5 donat gratis!! ");
+            System.out.println(" Silahkan pilih 5 donat yang anda inginkan! \n\n");
 
-          System.out.println(" Selamat anda mendapatkan promo beli 3 dessert box dapat 5 donat gratis!! ");
-          System.out.println(" Silahkan pilih 5 donat yang anda inginkan! \n\n");
-          super.menuDonat();
-          super.topingDonat();
-          System.out.println("Pilihan donat 1 :");
-          pilihanDonat = p.nextInt();
-          System.out.println("Pilihan donat 2 :");
-          pilihanDonat = p.nextInt();
-          System.out.println("Pilihan donat 3 :");
-          pilihanDonat = p.nextInt();
-          System.out.println("Pilihan donat 4 :");
-          pilihanDonat = p.nextInt();
-          System.out.println("Pilihan donat 5 :");
-          pilihanDonat = p.nextInt();
+        pelayanan.menuDonat();
+
     }
 
-    public void persen(int biaya)
-    {
-          System.out.println(" Selamat anda mendapatkan promo potongan harga sebesar 25k dengan transaksi min. 150k  ");
-          System.out.println(" Total biaya belanja anda adalah : " + (biaya - 25000));
-            
-    }
+    public void kurangiStok(int s)
+      {
+            // Kurangi stok donat
+            if(s == 1) {
+                  penyimpanan.getStokDonat("Donat Gula", 1);
+            }
+            else if(s == 2) {
+                  penyimpanan.getStokDonat("Donat Cokelat", 1);
+            }
+            else if(s == 3) {
+                  penyimpanan.getStokDonat("Donat Cokelat Putih", 1);
+            }
+            else if(s == 4) {
+                  penyimpanan.getStokDonat("Donat Matcha", 1);
+            }
+            else if(s == 5) {
+                  penyimpanan.getStokDonat("Donat Strawberry", 1);
+            }
+            else if(s == 6) {
+                  penyimpanan.getStokDonat("Donat Blueberry", 1);
+            }
+            else if(s == 7) {
+                  penyimpanan.getStokDonat("Donat Tiramisu", 1);
+            }
+            else if(s == 8) {
+                  penyimpanan.getStokDonat("Donat Cappuchino", 1);
+            }
+            else if(s == 9) {
+                  penyimpanan.getStokDonat("Donat Bomboloni Isi Cokelat", 1);
+            }
+            else if(s == 10) {
+                  penyimpanan.getStokDonat("Donat Bomboloni Isi Strawberry", 1);
+            }
+            else if(s == 11) {
+                  penyimpanan.getStokDonat("Donat Bomboloni Isi Blueberry", 1);
+            }
+            else if(s == 12) {
+                  penyimpanan.getStokDonat("Donat Selai Strawberry", 1);         
+            }
+            else if(s == 13) {
+                  penyimpanan.getStokDonat("Donat Selai Blueberry", 1);
+            }
+      }
+
+      public void pilihDonat(){
+
+            System.out.println("Pilihan donat 1 :");
+            pilihanDonat = p.nextInt();   
+            if(pilihanDonat >= 2 && pilihanDonat <= 8) {
+                  pelayanan.topingDonat();
+                  System.out.println("silahkan masukkan toping yang anda inginkan : ");
+                  pilihanToping = p.nextInt();
+            }
+            kurangiStok(pilihDonat);
+
+            System.out.println("Pilihan donat 2 :");
+            pilihanDonat = p.nextInt();   
+            if(pilihanDonat >= 2 && pilihanDonat <= 8) {
+                  pelayanan.topingDonat();
+                  System.out.println("silahkan masukkan toping yang anda inginkan : ");
+                  pilihanToping = p.nextInt();
+            }
+            kurangiStok(pilihDonat);
+      
+          
+            System.out.println("Pilihan donat 3 :");
+            pilihanDonat = p.nextInt();   
+            if(pilihanDonat >= 2 && pilihanDonat <= 8) {
+                  pelayanan.topingDonat();
+                  System.out.println("silahkan masukkan toping yang anda inginkan : ");
+                  pilihanToping = p.nextInt();
+            }
+            kurangiStok(pilihDonat);
+      
+          
+            System.out.println("Pilihan donat 4 :");
+            pilihanDonat = p.nextInt();   
+            if(pilihanDonat >= 2 && pilihanDonat <= 8) {
+                  pelayanan.topingDonat();
+                  System.out.println("silahkan masukkan toping yang anda inginkan : ");
+                  pilihanToping = p.nextInt();
+            }
+            kurangiStok(pilihDonat);
+
+            System.out.println("Pilihan donat 5 :");
+            pilihanDonat = p.nextInt();   
+            if(pilihanDonat >= 2 && pilihanDonat <= 8) {
+                  pelayanan.topingDonat();
+                  System.out.println("silahkan masukkan toping yang anda inginkan : ");
+                  pilihanToping = p.nextInt();
+            }
+            kurangiStok(pilihDonat);
+      
+      }
+
+      public void potonganHarga(int biaya)
+      {
+            if (biaya > 150000) {
+                  setBiaya(biaya);
+                  System.out.println(" Selamat anda mendapatkan promo potongan harga sebesar 25k dengan transaksi min. 150k  ");
+            }         
+      }
+
+      public void setBiaya(int biaya)
+      {
+          this.biaya = biaya;
+      }
+
+      public int getBiaya()
+      {
+          return this.biaya -= 25000;
+      }
 }
     
 
